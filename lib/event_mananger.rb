aebea39a058c556abccfd9096b99b717d03ebc85
SheaDevop <nacho.dieguez18@gmail.com>
2022-03-28 18:01:03 -0300

    EventMananger completed

---
+++ lib/event_mananger.rb
@@ -0,0 +1,76 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'time'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def clean_phone_numbers(phone_number)
+    phone_number = phone_number.to_s.gsub("-", "")
+    if phone_number.length == 11 && phone_number.start_with?("1")
+        phone_number[1..10]
+    elsif phone_number.length != 10
+        phone_number = "0000000000"
+    else 
+        phone_number
+    end
+end
+
+def check_for_register_hour(register_time, format="%-m/%d/%Y %k:%M")
+    time = Time.strptime(register_time, format)
+    time.hour
+end
+
+def check_for_register_dotw(register_time, format="%-m/%d/%Y %k:%M")
+    time = Time.strptime(register_time, format)
+    register_day = time.strf("%A")
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
+end
